efbd3783b8ebc7d42c7b231c9a6480e34d5b4606
Etienne <etienne.bardet@gmail.com>
2024-10-26 10:22:18 +0200

    code cleanup

--- network.py
+++ network.py
@@ -28,23 +28,20 @@ class Modele(nn.Module):
         self.avg_pool4 = nn.AdaptiveAvgPool2d((16,16))
         
     # ATTENTION BLOCK
-        self.attention_block = Self_Attn(in_dim = out_channels * 16)
+        self.attention_block = Self_Attn(in_dim = out_channels * 16, with_attn=True)
                 
     # DECODEUR
         
         self.convolutional_block_decodeur1 = Convolutional_Block(out_channels*16 , out_channels *8)
         self.convolutional_block_decodeur2 = Convolutional_Block(out_channels *8 , out_channels *4)
         
-        self.upsampling2 = nn.UpsamplingNearest2d(scale_factor=2) # si pas bon result changer le upsampling
         self.convolutional_block_decodeur3 = Convolutional_Block(out_channels *4 , out_channels*2)
         
-        self.upsampling3 = nn.UpsamplingNearest2d(scale_factor=2) # si pas bon result changer le upsampling
         self.convolutional_block_decodeur4 = Convolutional_Block(out_channels*2 , out_channels)
         
-        self.upsampling4 = nn.UpsamplingNearest2d(scale_factor=2) # si pas bon result changer le upsampling
         self.convolutional_block_decodeur5 = Convolutional_Block(out_channels , 1)
 
-        self.upsampling5 = nn.UpsamplingNearest2d(scale_factor=2) # si pas bon result changer le upsampling
+        self.upsampling = nn.UpsamplingBilinear2d(scale_factor=2) # si pas bon result changer le upsampling
         #upsampling pour augmenter taille puis pour les channels: (conv ou dedans on divise par 2 le nombre de channels) 
         
     # SIGMOID
@@ -82,7 +79,7 @@ class Modele(nn.Module):
         output_encoder_combined = torch.concat((avg_pool_4_im1, avg_pool_4_im2), dim=1) #Concat on channels
         
         # Attention : image 1 + image 2
-        attention = self.attention_block(output_encoder_combined) # MAYBE WE NEED TO CONCATENATE RATHEN THAN JUST ADDED THE TWO TERMS
+        attention, mask = self.attention_block(output_encoder_combined) # MAYBE WE NEED TO CONCATENATE RATHEN THAN JUST ADDED THE TWO TERMS
         #C'est pas un maybe, il faut le faire comme ca lol
 
 
@@ -91,15 +88,15 @@ class Modele(nn.Module):
         conv_block_1_dec = self.convolutional_block_decodeur1(attention)
         conv_block_2_dec = self.convolutional_block_decodeur2(conv_block_1_dec)
         
-        upsampling2 =self.upsampling2(conv_block_2_dec)
+        upsampling2 =self.upsampling(conv_block_2_dec)
         conv_block_3_dec=self.convolutional_block_decodeur3(upsampling2)
         
-        upsampling3 =self.upsampling3(conv_block_3_dec)
+        upsampling3 =self.upsampling(conv_block_3_dec)
         conv_block_4_dec=self.convolutional_block_decodeur4(upsampling3)
         
-        upsampling4 =self.upsampling4(conv_block_4_dec)
+        upsampling4 =self.upsampling(conv_block_4_dec)
         conv_block_5_dec=self.convolutional_block_decodeur5(upsampling4)
         
         output_final = self.sigmoid(conv_block_5_dec)
 
-        return output_final
+        return output_final, mask
